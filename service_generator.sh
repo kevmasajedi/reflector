70246a768c5227099b19995082dcc1f4e6398c97
kevmasajedi <87088715+kevmasajedi@users.noreply.github.com>
2024-09-10 15:26:44 +0330

    V0.1

--- service_generator.sh
+++ service_generator.sh
@@ -26,16 +26,16 @@ WantedBy=multi-user.target
 "
 
 echo "Creating service file for $SERVICE_NAME at /etc/systemd/system/$SERVICE_NAME.service"
-echo "$SERVICE_FILE_CONTENT" | sudo tee /etc/systemd/system/$SERVICE_NAME.service > /dev/null
+echo "$SERVICE_FILE_CONTENT" |  tee /etc/systemd/system/$SERVICE_NAME.service > /dev/null
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
-sudo systemctl enable $SERVICE_NAME.service
+systemctl enable $SERVICE_NAME.service
 
-sudo systemctl start $SERVICE_NAME.service
+systemctl start $SERVICE_NAME.service
 
 echo "Service status:"
-sudo systemctl status $SERVICE_NAME.service
+systemctl status $SERVICE_NAME.service
 
 # Output message
 echo "Service $SERVICE_NAME created, started, and enabled at boot successfully."
